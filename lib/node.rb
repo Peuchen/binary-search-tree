e4d57e4bf1096916f8d2d421c66c8b96d45e3dfc
peuchen <npeuchen@gmail.com>
2020-12-20 17:57:42 +0100

    Add Node class to lib directory

---
+++ lib/node.rb
@@ -0,0 +1,16 @@
+class Node
+  include 'Comparable'
+  attr_accessor :value
+
+  def initialize(value, left_child, right_child)
+    @value = value
+
+    @left_child = left_child
+    @right_child = right_child
+  end
+
+  def <=>(other_node)
+    self.value <=> other_node.value
+  end
+
+end
