b9a192f34b92c07205968ff14618b1cf9c4e4907
peuchen <npeuchen@gmail.com>
2020-12-28 21:53:45 +0100

    Create insert method

--- lib/binary-search-tree.rb
+++ lib/binary-search-tree.rb
@@ -3,4 +3,11 @@ require_relative 'tree'
 
 array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 tree = Tree.new(array)
+
+puts "BEFORE INSERTION"
+tree.pretty_print
+
+tree.insert(12)
+
+puts "AFTER INSERTION"
 tree.pretty_print
--- lib/tree.rb
+++ lib/tree.rb
@@ -1,7 +1,7 @@
 class Tree
   attr_reader :data, :root
 
-  def initialize(array)
+  def initialize(array = [])
     @data = array.sort.uniq
     @root = build_tree(data)
   end
@@ -18,7 +18,28 @@ class Tree
     node
   end
 
-  def pretty_print(node = @root, prefix = '', is_left = true)
+  def insert(value, node = root)
+    return Node.new(value) if node.nil?
+    return nil if node.value == value
+
+    if value < node.value
+      if node.left.nil?
+        node.left = Node.new(value)
+      else
+        insert(value, node.left)
+      end
+    else
+      if node.right.nil?
+        node.right = Node.new(value)
+      else
+        insert(value, node.right)
+      end
+    end
+
+    node
+  end
+
+  def pretty_print(node = root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
